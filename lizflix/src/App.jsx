dabce85c94cd1f204753aba1176d5f47bffe3931
jzsantana <juliass7187@gmail.com>
2023-09-13 11:23:44 -0300

    update

---
+++ lizflix/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import { Outlet } from 'react-router-dom'
+import Navbar from './components/navbar'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='App'>
+      <Navbar/>
+      <Outlet/>
+    </div>
+  )
+}
+
+export default App
